b2d17edab6763f65905ae5afe8bca3be01446218
Esteban Volveras <s.t.ban1401@gmail.com>
2020-09-22 21:08:04 -0500

    Se creó la interacción de zoom

--- src/scripts/zoom.js
+++ src/scripts/zoom.js
@@ -1,11 +1,38 @@
-const image = document.querySelector(".feature__image-zoom");
+// Zoom interaction
+// Based on https://daily-dev-tips.com/posts/vanilla-javascript-image-magnify-%F0%9F%94%8E/
+const zoomContainer = document.querySelector(".feature__interaction--zoom");
+const original = document.querySelector(".feature__image");
+const magnified = document.querySelector(".feature__zoom-image");
 
-function handleZoom () {
-    if(!image.classList.contains("feature__image-zoom--zoom-in")) {
-        image.classList.add("feature__image-zoom--zoom-in");
-    } else {
-        image.classList.remove("feature__image-zoom--zoom-in");
-    }
+function handleZoom(e) {
+    var style = magnified.style,
+        x = e.pageX - this.offsetLeft,
+        y = e.pageY - this.offsetTop,
+        imgWidth = original.width,
+        imgHeight = original.height,
+        xperc = (x / imgWidth) * 100,
+        yperc = (y / imgHeight) * 100;
+
+    // Set the background of the magnified image horizontal
+    style.backgroundPositionX = xperc + '%';
+    // Set the background of the magnified image vertical
+    style.backgroundPositionY = yperc + '%';
+
+    // Move the magnifying glass with the mouse movement.
+    style.left = x - magnified.offsetWidth / 2 + 'px';
+    style.top = y - magnified.offsetHeight / 2 + 'px';
 }
 
-image.addEventListener("click", handleZoom)
+zoomContainer.addEventListener('mousemove', handleZoom);
+
+
+// Slider Interaction
+const slider = document.querySelector('.feature__slider');
+const img = document.querySelector('.feature__image--compare:last-child');
+
+function handleSlider () {
+  console.log('hola', slider.value);
+  // img.style.opacity = slider.value;
+  img.style.width = (slider.value * 100) + '%';
+}
+slider.addEventListener('input', handleSlider);
