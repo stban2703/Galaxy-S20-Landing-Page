a4e94c48ad302763efd8fe9360d919b8c173fe65
Esteban Volveras <s.t.ban1401@gmail.com>
2020-09-23 02:10:01 -0500

    Se crearon las interacciones del scroll y se agregaron href

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
+// Scroll interactions
+const header = document.querySelector(".header--sub");
+const gallery = document.querySelector(".gallery");
+const specs = document.querySelector(".specs");
+const navTexts = document.querySelectorAll(".section-nav--sub .section-nav__text");
+
+function handleHeader() {
+  if (window.scrollY > header.offsetTop) {
+    header.classList.add("header--fixed");
+  } else {
+    header.classList.remove("header--fixed");
+  }
+}
+window.addEventListener('scroll', handleHeader);
+
+
+navTexts.forEach(function (elem, index) {
+  window.addEventListener('scroll', function() {
+    if(window.scrollY < gallery.offsetTop - 100) {
+      navTexts[0].classList.add('section-nav__text--current');
+      navTexts[1].classList.remove('section-nav__text--current');
+      navTexts[2].classList.remove('section-nav__text--current');
+
+    } else if (window.scrollY < specs.offsetTop - 100) {
+      navTexts[0].classList.remove('section-nav__text--current');
+      navTexts[1].classList.add('section-nav__text--current');
+      navTexts[2].classList.remove('section-nav__text--current');
+
+    } else {
+      navTexts[0].classList.remove('section-nav__text--current');
+      navTexts[1].classList.remove('section-nav__text--current');
+      navTexts[2].classList.add('section-nav__text--current');
+    }
+  });
+});
+
+
+
 // Zoom interaction
 // Based on https://daily-dev-tips.com/posts/vanilla-javascript-image-magnify-%F0%9F%94%8E/
 const zoomContainer = document.querySelector(".feature__interaction--zoom");
@@ -5,22 +43,22 @@ const original = document.querySelector(".feature__image");
 const magnified = document.querySelector(".feature__zoom-image");
 
 function handleZoom(e) {
-    var style = magnified.style,
-        x = e.pageX - this.offsetLeft,
-        y = e.pageY - this.offsetTop,
-        imgWidth = original.width,
-        imgHeight = original.height,
-        xperc = (x / imgWidth) * 100,
-        yperc = (y / imgHeight) * 100;
+  var style = magnified.style,
+    x = e.pageX - this.offsetLeft,
+    y = e.pageY - this.offsetTop,
+    imgWidth = original.width,
+    imgHeight = original.height,
+    xperc = (x / imgWidth) * 100,
+    yperc = (y / imgHeight) * 100;
 
-    // Set the background of the magnified image horizontal
-    style.backgroundPositionX = xperc + '%';
-    // Set the background of the magnified image vertical
-    style.backgroundPositionY = yperc + '%';
+  // Set the background of the magnified image horizontal
+  style.backgroundPositionX = xperc + '%';
+  // Set the background of the magnified image vertical
+  style.backgroundPositionY = yperc + '%';
 
-    // Move the magnifying glass with the mouse movement.
-    style.left = x - magnified.offsetWidth / 2 + 'px';
-    style.top = y - magnified.offsetHeight / 2 + 'px';
+  // Move the magnifying glass with the mouse movement.
+  style.left = x - magnified.offsetWidth / 2 + 'px';
+  style.top = y - magnified.offsetHeight / 2 + 'px';
 }
 
 zoomContainer.addEventListener('mousemove', handleZoom);
@@ -30,7 +68,7 @@ zoomContainer.addEventListener('mousemove', handleZoom);
 const slider = document.querySelector('.feature__slider');
 const img = document.querySelector('.feature__image--compare:last-child');
 
-function handleSlider () {
+function handleSlider() {
   console.log('hola', slider.value);
   // img.style.opacity = slider.value;
   img.style.width = (slider.value * 100) + '%';
@@ -47,7 +85,7 @@ let current = 0;
 
 backButton.addEventListener('click', function () {
   current--;
-  if(current < 0) {
+  if (current < 0) {
     current = galleryStripe.children.length - 1;
   }
   const width = galleryContainer.clientWidth;
@@ -57,7 +95,7 @@ backButton.addEventListener('click', function () {
 
 nextButton.addEventListener('click', function () {
   current++;
-  if(current >= galleryStripe.children.length) {
+  if (current >= galleryStripe.children.length) {
     current = 0;
   }
   const width = galleryContainer.clientWidth;
