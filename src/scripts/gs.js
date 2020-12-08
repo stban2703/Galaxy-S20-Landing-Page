3535d3ad3cee4efb2234bc2787522c15033a5133
stban2703 <s.t.ban1401@gmail.com>
2020-12-08 15:40:44 -0500

    Se agregó transición de modo noche

--- src/scripts/gs.js
+++ src/scripts/gs.js
@@ -46,8 +46,9 @@ window.addEventListener('load', function () {
         gsap.to(elem, {
             xPercent: -100,
             scrollTrigger: {
+                scrub: true,
                 trigger: elem,
-                delay: 5,
+                delay: 1,
                 toggleActions: "restart pause reverse pause"
             },
             duration: 1.5,
@@ -60,8 +61,9 @@ window.addEventListener('load', function () {
         gsap.to(elem, {
             xPercent: 100,
             scrollTrigger: {
+                scrub: true,
                 trigger: elem,
-                delay: 5,
+                delay: 1,
                 toggleActions: "restart pause reverse pause"
             },
             duration: 1.5,
@@ -95,6 +97,28 @@ window.addEventListener('load', function () {
         backgroundColor: "#000000",
         duration: 3,
         ease: "in"
+    });
+
+
+    // Modo noche transicion
+    gsap.utils.toArray(".comparisonSection").forEach(section => {
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: section,
+                start: "center center",
+                // makes the height of the scrolling (while pinning) match the width, thus the speed remains constant (vertical/horizontal)
+                end: () => "+=" + section.offsetWidth,
+                scrub: true,
+                pin: true,
+                anticipatePin: 1,
+                pinSpacing: section.offsetWidth
+            },
+            defaults: { ease: "none" }
+        });
+        // animate the container one way...
+        tl.fromTo(section.querySelector(".afterImage"), { xPercent: 100, x: 0 }, { xPercent: 0 })
+            // ...and the image the opposite way (at the same time)
+            .fromTo(section.querySelector(".afterImage img"), { xPercent: -100, x: 0 }, { xPercent: 0 }, 0)
     });
 
 });
